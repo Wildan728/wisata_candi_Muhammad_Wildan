4db6e9b1ef3c3d8dc201eb735e30db28d46d625a
Wildan728 <shahabwildan1225@gmail.com>
2024-11-18 12:51:33 +0700

    Gesture Detectore

--- wisata_candi/lib/widgets/item_card.dart
+++ wisata_candi/lib/widgets/item_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/Screens/detail_screen.dart';
 import 'package:wisata_candi/models/candi.dart';
 
 class ItemCard extends StatelessWidget {
@@ -8,45 +9,56 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari cari
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15)),
-      margin: EdgeInsets.all(4),
-      elevation: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.asset(
-                  candi.imageAsset, 
-                  width: double.infinity, 
-                  fit: BoxFit.cover
+    // TODO: 6. Implementasi routing ke DetailScreen
+    return InkWell(
+      onTap: (){
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DetailScreen(candi: candi),
+            ),
+        );
+      },
+      child: Card(
+        // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari cari
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15)),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // TODO: 3. Buat Image sebagai anak dari Column
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    fit: BoxFit.cover
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              candi.name,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, bottom: 8),
-            child: Text(
-                candi.type,
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              // TODO: 4. Buat Text sebagai anak dari Column
+              child: Text(
+                candi.name,
                 style: TextStyle(
-                    fontSize: 12),
+                    fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          // TODO: 3. Buat Image sebagai anak dari Column
-          // TODO: 4. Buat Text sebagai anak dari Column
-          // TODO: 5. Buat Text sebagai anak dari Column
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 16, bottom: 8),
+              // TODO: 5. Buat Text sebagai anak dari Column
+              child: Text(
+                  candi.type,
+                  style: TextStyle(
+                      fontSize: 12),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
